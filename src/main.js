24e766d431bdd2ee674ac7e2c8bc1257a530084e
Gabriela Victorio <lucerovp16@gmail.com>
2022-02-24 17:21:57 -0500

    funcion

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+function n(cadena) {
+    var n1= "hola";
+    return console.log(n1)
+}
