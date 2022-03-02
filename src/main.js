00ae88ecdd2578b6b8b0caf632e28bb297788c48
Gabriela Victorio <lucerovp16@gmail.com>
2022-03-01 22:14:59 -0500

    creando funcion al mostrar

--- src/main.js
+++ src/main.js
@@ -1,30 +1,49 @@
-import { example } from './data.js';
+import { searchPokemon } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //console.log(example, data);
-console.log(example, data.pokemon[0].num);
-//console.log(example, data.pokemon[0].generation.num);
-//console.log(example, data.pokemon[0].img);
+//console.log(nameSearch, data.pokemon[0].num);
+let button=document.getElementById("btn");
+let search=document.getElementById("search").value;
+let poke=document.getElementById("pokemon-container");
+function mostrar(){
+  let poken;
+  for (let properties of data.pokemon) {
+    poken=poke.innerHTML += `
+    <div class="container">
+      <div class="card">
+        <img src=${properties.img}>
+      </div>
+      <div class="information">
+        <h4><p class="number-pokemon">N° ${properties.num}</p> </h4>
+        <p class="pokemon-name"> ${properties.name} </p>
+        <p class="type-pokemon">${properties.type}</p>
+      <div>
+    <div>`; 
 
-//data.pokemon.forEach(function(properties) {
-  //  document.body.innerHTML += "<p>"+properties.name+"</p> <img src='"+properties.img+"'></img><br>"
-//})
-//document.getElementById("window").innerHTML=data.pokemon[0].num;
-//document.getElementById("window").innerHTML=`<img src=${data.pokemon[0].img}>`
-
-for (let properties of data.pokemon) {
-document.getElementById("pokemon-container").innerHTML += `
-<div class="container">
-  <div class="card">
-    <img src=${properties.img}>
-  </div>
-  <div class="information">
-    <h4><p class="number-pokemon">N° ${properties.num}</p> </h4>
-    <p class="pokemon-name"> ${properties.name} </p>
-    <p class="type-pokemon">${properties.type}</p>
-  <div>
-<div>`; 
 }
+return poken;
+
+}
+mostrar();
+button.addEventListener("click",teclado)
+//search.addEventListener("keypress",teclado)
+function teclado(){
+  if(search==""){
+    mostrar()
+  }else{
+    return console.log(searchPokemon(mostrar(),search));
+  }
+
+}
+
+
+
+
+
+
+
+
 
